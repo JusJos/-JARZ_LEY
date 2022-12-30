d5ee37a513f9bc976637d7b77f8520f05ad51eda
Justin Joseph <91304818+JusJos@users.noreply.github.com>
2022-12-31 00:55:52 +0530

    Create 339. SelectionSort_Recursion.cpp

---
+++ LEETS/339. SelectionSort_Recursion.cpp
@@ -0,0 +1,21 @@
+int sort(int arr[],int x){
+
+    if(x==0){
+        return arr[0];
+    }
+    int temp=0;
+    for (int i = 0; i<x; i++)
+    {    
+         
+        if(arr[i]<arr[temp]){
+            temp=i;}
+    }
+    swap(arr[0],arr[temp]);
+
+    return sort(arr+1,x-1);
+    
+
+
+    
+
+}
