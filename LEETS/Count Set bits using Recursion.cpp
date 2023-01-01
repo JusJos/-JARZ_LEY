3b2523186227f504a05700db3a5daeb469979628
Justin Joseph <91304818+JusJos@users.noreply.github.com>
2023-01-01 16:56:24 +0530

    Update Count Set bits using Recursion.cpp

--- LEETS/Count Set bits using Recursion.cpp
+++ LEETS/Count Set bits using Recursion.cpp
@@ -1,26 +1,13 @@
-#include<iostream>
-using namespace std;
-
-
-int bit(int x){
+int Set_Bits(int x){
 
 if(x==0){
     return 0;
 }
 
 if(x&1)
-    return 1+bit(x>>1);
+    return 1+Set_Bits(x>>1);
 else
-    return bit(x>>1);
+    return Set_Bits(x>>1);
 
 }
 
-
-
-int main()
-{ int n =1;
-    int ans=bit(n);
-    cout<<ans;
-    
-    return 0;
-}
