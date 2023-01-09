1d7594ddb0992f8b014a3109926cd7a19b40724d
Justin Joseph <91304818+JusJos@users.noreply.github.com>
2023-01-09 19:15:11 +0530

    Create Modified_Binary_Search_in a once rotated array.cpp

---
+++ LEETS/Modified_Binary_Search_in a once rotated array.cpp
@@ -0,0 +1,26 @@
+int search(int A[], int l, int h, int key){
+        
+        while(l<=h){
+        int mid=l+(h-l)/2;
+            
+            if(A[mid]==key){
+                return mid;
+            }
+            //left
+            if(A[l]<=A[mid]){
+                
+                if(key>=A[l]&&key<A[mid]){
+                    h=mid-1;
+                }else{
+                    l=mid+1;
+                }
+              //right  
+            }else{
+                if(key>A[mid]&&key<=A[h]){
+                    l=mid+1;
+                }else
+                    h=mid-1;
+            }
+                
+                
+            }
