aa8797b3bafad2b26b3c34866130b4ae7536ccb8
Justin Joseph <91304818+JusJos@users.noreply.github.com>
2023-01-12 00:58:15 +0530

    Create PowerOfTwo.cpp

---
+++ LEETS/PowerOfTwo.cpp
@@ -0,0 +1,29 @@
+// Time Complexity -> O(log(n))
+bool isPowerofTwo(long long n){
+        
+        long long k=n;
+        long long d=2;
+        if(n==0){
+            return false;
+       }
+        if(n==1){
+            return true;
+        }
+        while(d<k){
+            if(k%d!=0){
+                return false;
+            }else{
+                d=d*2;
+            }
+        }
+        if(d==k){
+            return true;}
+        else{
+            return false;
+        }
+        
+   
+        
+    }
+
+// Time Complexity -> O(1)
