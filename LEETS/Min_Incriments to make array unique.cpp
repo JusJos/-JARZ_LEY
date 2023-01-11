68d1a426b43b29e16cc30f7ddd789947222c9122
Justin Joseph <91304818+JusJos@users.noreply.github.com>
2023-01-11 16:56:28 +0530

    Create Min_Incriments to make array unique.cpp

---
+++ LEETS/Min_Incriments to make array unique.cpp
@@ -0,0 +1,17 @@
+long long int minIncrements(vector<int> arr, int N) {
+        int ans=0,temp=0;
+        
+        sort(arr.begin(),arr.end());
+        for(int i=1;i<N;i++){
+            if(arr[i]<=arr[i-1]){
+                temp=arr[i-1]+1-arr[i];
+                ans=ans+temp;
+                arr[i]=arr[i-1] +1;
+            }
+                //cout<<ans<<"->";
+            
+            
+        }
+        
+        
+    return ans;}
