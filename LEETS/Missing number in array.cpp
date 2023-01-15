1a76583a4a6948c5a5cad67fd78673c4c2932f07
Justin Joseph <91304818+JusJos@users.noreply.github.com>
2023-01-15 23:25:55 +0530

    Create Missing number in array.cpp

---
+++ LEETS/Missing number in array.cpp
@@ -0,0 +1,10 @@
+int MissingNumber(vector<int>& array, int n) {
+        long long sum=(n)*(n+1)/2;
+        for(int i=0;i<n-1;i++){
+            sum-=array[i];
+            
+        }
+        
+        return sum;
+       
+    }
