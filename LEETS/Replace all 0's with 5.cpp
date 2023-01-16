c8b48524f308975c96800f5e23dfb4b0ba6781cd
Justin Joseph <91304818+JusJos@users.noreply.github.com>
2023-01-16 21:35:37 +0530

    Create Replace all 0's with 5.cpp

---
+++ LEETS/Replace all 0's with 5.cpp
@@ -0,0 +1,9 @@
+int convertFive(int n)
+    {   int k=n;
+        int i=0;
+        while(k>0){
+            if(k%10==0){
+            n+=5*pow(10,i);}
+            k=k/10;
+        i++;}
+    return n;}
