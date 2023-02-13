b0b1fdba916f566ab5b7a52ae05a20cff2361cc7
Justin Joseph <91304818+JusJos@users.noreply.github.com>
2023-02-13 18:03:49 +0530

    Create Convert array into Zig-Zag fashion.cpp

---
+++ LEETS/Convert array into Zig-Zag fashion.cpp
@@ -0,0 +1,14 @@
+void zigZag(int arr[], int n) {
+        bool flag=false;
+        for(int i=1;i<n;i++){
+            if(flag){
+                if(arr[i-1]<arr[i]){
+                    swap(arr[i-1],arr[i]);
+                }
+                
+            }else if(arr[i-1]>arr[i]){
+                swap(arr[i-1],arr[i]);
+            }
+                flag=!flag;
+        }
+    }
