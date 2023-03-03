677e20c006735c713b48e634da88d483e7289cdd
Justin Joseph <91304818+JusJos@users.noreply.github.com>
2023-03-03 10:50:06 +0530

    Update PowerOfTwo.cpp

--- LEETS/PowerOfTwo.cpp
+++ LEETS/PowerOfTwo.cpp
@@ -20,8 +20,7 @@ bool isPowerofTwo(long long n){
             return true;}
         else{
             return false;
-        }
-        }
+        }}
 // Time Complexity -> O(1)
 bool isPowerofTwo(long long n){
  return (n != 0) && ((n & (n - 1)) == 0);   
