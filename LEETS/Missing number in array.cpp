529ff0adaf3d6de750e5a3b66b5f889c0daa5797
Shivesh Raj <103254362+theshiveshh@users.noreply.github.com>
2023-04-21 20:53:07 +0530

    Update Missing number in array.cpp

--- LEETS/Missing number in array.cpp
+++ LEETS/Missing number in array.cpp
@@ -1,8 +1,7 @@
 int MissingNumber(vector<int>& array, int n) {
         long long sum=(n)*(n+1)/2;
         for(int i=0;i<n-1;i++){
-            sum-=array[i];
-            
+            sum-=array[i]; 
         }
         
         return sum;
